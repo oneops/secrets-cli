8e5687518736c1bb7d1589af09ccc3e47ff96720
vn0ytch <vagrawal0@walmart.com>
2020-10-11 12:29:30 -0700

    PLONEOPS-19247 : updated code

--- src/main/java/com/oneops/secrets/command/Console.java
+++ src/main/java/com/oneops/secrets/command/Console.java
@@ -10,15 +10,15 @@ public class Console {
 
   static final String INPUT = "^y(es)?$";
 
-  static void readConsole(String message)  throws IOException{
+  static void readConsole(String message) throws IOException {
     Pattern p = Pattern.compile(INPUT);
     String in;
     if (null != System.console()) {
       in = System.console().readLine(warn(message));
     } else {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        in = reader.readLine();
-        System.out.println(warn(message) + in);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      System.out.println(warn(message));
+      in = reader.readLine();
     }
     if (in == null || !p.matcher(in.toLowerCase()).matches()) {
       throw new IllegalStateException("Exiting");
