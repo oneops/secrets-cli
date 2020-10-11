357e4f04b5a2bcf5056df79a2f713d50c9c4a646
vn0ytch <vagrawal0@walmart.com>
2020-10-11 11:24:34 -0700

    PLONEOPS-19247 : Removed try catch block

--- src/main/java/com/oneops/secrets/command/Console.java
+++ src/main/java/com/oneops/secrets/command/Console.java
@@ -2,6 +2,7 @@ package com.oneops.secrets.command;
 
 import static com.oneops.secrets.utils.Color.warn;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
@@ -9,19 +10,15 @@ public class Console {
 
   static final String INPUT = "^y(es)?$";
 
-  static void readConsole(String message) {
+  static void readConsole(String message)  throws IOException{
     Pattern p = Pattern.compile(INPUT);
     String in;
     if (null != System.console()) {
       in = System.console().readLine(warn(message));
     } else {
-      try {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         in = reader.readLine();
         System.out.println(warn(message) + in);
-      } catch (Exception e) {
-        throw new IllegalStateException("Exiting");
-      }
     }
     if (in == null || !p.matcher(in.toLowerCase()).matches()) {
       throw new IllegalStateException("Exiting");
