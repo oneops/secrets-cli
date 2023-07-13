5d83533081259b9e137ea4c4d81418bce059d711
suresh590 <138920337+suresh590@users.noreply.github.com>
2023-07-13 12:31:32 -0500

    PLONEOPS-23869: Setup secret-client for OneOps PCI (#13)
    * PLONEOPS-23869: Setup secret-client for OneOps PCI
    
    * PLONEOPS-23869: Removing the tekton domain changes for secret-client in OneOps PCI

--- src/main/java/com/oneops/secrets/config/AuthDomain.java
+++ src/main/java/com/oneops/secrets/config/AuthDomain.java
@@ -10,12 +10,11 @@ public enum AuthDomain {
   MGMT("mgmt"),
   STG("stg"),
   DEV("dev"),
-
-  /*Tekton domain name*/
-  TEKTONPROD("tektonprod"),
-  TEKTONMGMT("tektonmgmt"),
-  TEKTONSTG("tektonstg"),
-  TEKTONDEV("tektondev");
+  
+/*Oneops PCI domain name*/
+  OOPCIQA("stg-pci"),
+  OOPCIPROD("prod-pci"),
+  OOPCIMGMT("mgmt-pci");
 
   private final String type;
 
